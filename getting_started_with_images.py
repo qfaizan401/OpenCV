c7144cebf15a46bbb9d85571deb0f6ef870655db
qfaizan401 <qfaizan401@gmail.com>
2019-09-08 17:42:27 +0500

    1st try

---
+++ getting_started_with_images.py
@@ -0,0 +1,18 @@
+#read image from a file
+
+import cv2
+import numpy as np
+img = cv2.imread(filename='lena.jpg', flags=1)
+# 1 for coloured img
+# 0 for grayscale img
+# -1 for unchange img
+print(img) # print the matrics of img
+cv2.imshow(winname='lena', mat=img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print(f'\nThe shape of image Lena is {np.shape(img)}')
+
+#write an img to a file
+
+cv2.imwrite(filename='copy_of_lena.png',img=img)
